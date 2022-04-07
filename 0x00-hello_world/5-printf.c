efcd5841843a152cefd271b8546699144a2e2a7f
Yonathan <yonit.worktasks@gmail.com>
2022-04-07 12:48:42 -0700

    Fixes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * My hello, printf task
  * Return: return 1 after program execution
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
