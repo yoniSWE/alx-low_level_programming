fd02e92e9524948bb95c6c3ba75a850c3942f69d
Yonathan <yonit.worktasks@gmail.com>
2022-04-11 01:39:33 -0700

    Fifth Q

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Prints all single digit numbers starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int b;
 
+	for (c = 0; c < 10; c++)
+		printf("%d", c);
+
+	return (0);
+}
