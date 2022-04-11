73f331a9ee22a96b74f916e242277f75e9a4bb7a
Yonathan <yonit.worktasks@gmail.com>
2022-04-11 01:56:23 -0700

    Eighth task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Description: Print all numbers of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c >= 9; c++)
+		putchar('0' + c);
+
+	for (c = 0; c < 6; c++)
+		putchar('a' + c);
+
+	putchar('\n');
+
+	return (0);
+}
