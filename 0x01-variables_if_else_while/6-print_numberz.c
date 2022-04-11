5f4e82097d7b7a9463ac24e660d42f027b50cb43
Yonathan <yonit.worktasks@gmail.com>
2022-04-11 01:45:33 -0700

    6th Q

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Prints all single digit numbers starting from 0 using
+ * putchar only
+ * Return: 0
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+		putchar('' + b)
+
+	putchar("\n");
+
+	return (0);
+}
