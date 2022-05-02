39803229191893b7335792f855e3bd70def48363
Yonathan <yonit.worktasks@gmail.com>
2022-05-02 07:19:03 -0700

    Fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,6 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
+
 	if (argc == 3)
 	{
 		a = atoi(argv[1]);
@@ -18,6 +19,7 @@ int main(int argc, char *argv[])
 		printf("%d\n", a *b);
 		return (0);
 	}
+
 	printf("Error\n");
 	return (1);
 }
